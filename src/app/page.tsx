015f89dfd8c70906d4b120ea233228cd8dfd2f28
AishaJunaid607 <aishajunaid351@gmail.com>
2024-11-03 22:05:59 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import UnitConverterComponent from "@/component/Unit-converter";
+export default function Home() {
+  return (
+   <div>
+    <UnitConverterComponent />
+   </div>
+  );
+}
